815224714d9d2e045e0eff45714caf4023af656e
Senya Semin <senyasemi@yandex.ru>
2021-03-24 00:29:41 +0300

    Better clusters

--- functions.py
+++ functions.py
@@ -2,9 +2,13 @@ from typing import Any, List, Tuple
 
 import numpy as np
 import scipy.stats as stats
-from sklearn import metrics
-from sklearn.cluster import KMeans
 
+from sklearn import metrics
+from sklearn.cluster import DBSCAN
+from sklearn.neighbors import NearestNeighbors
+
+import seaborn as sns
+import matplotlib.pyplot as plot
 
 def rs(array: np.array, step: int) -> float:
     def compose(array: np.array, step: int) -> np.array:
@@ -55,35 +59,69 @@ def medium(data):
 
 def clusters(hurst, stability):
     vector = np.array(list(zip(hurst, stability)))
-    k = (
-        np.argmax(
-            [
-                metrics.calinski_harabasz_score(vector, KMeans(n_clusters=n).fit(vector).labels_)
-                for n in np.arange(2, 9)
-            ]
-        )
-        + 1
-    )
-    return KMeans(n_clusters=k).fit(vector).labels_
+    nn = NearestNeighbors(n_neighbors=5).fit(vector)
+    distances, idx = nn.kneighbors(vector)
+    distances = np.sort(distances, axis=0)[:, 1]
+#    sns.lineplot(x= range(len(distances)), y =distances)
+#    plot.show()
+#    eps = float(input("Пожалуйста введите расстояние между точками"))
+    return DBSCAN(eps=0.01, min_samples= 5).fit(vector).labels_
 
-
-def attractor(data, clusters, a, b):
+def attractor_(data, clusters, a = 1.4, b = 0.3, step = 0):
     def henon(data, a, b):
         x, y = [], []
         for i in range(len(data)):
             x += [[1 - a * data[i][:-1][j] ** 2 + data[i][1:][j] for j in range(len(data[i]) - 1)]]
             y += [[b * data[i][:-1][j] for j in range(len(data[i]) - 1)]]
         return x, y
+    
+    def clear(xx, yy,a = a, b=b, step = step):
+        x = np.copy(xx)
+        y = np.copy(yy)
+        for i in range(20):
+            x_, y_ = [], []
+            for j in range(len(x)):
+                x_ += [[1 - a * xx[j][k] ** 2 + yy[j][k] for k in range(len(x[j]) - 1)]]
+                y_ += [[b * xx[j][k] for k in range(len(x[j]) - 1)]]
+            xx = x_
+            yy= y_
+        xx = [np.where(np.isfinite(xx[i]) == False) for i in range(len(xx))]
+        yy = [np.where(np.isfinite(yy[i]) == False) for i in range(len(yy))]
+        x = [np.delete(x[i], j) for i in range(len(x)) for j in xx[i]]
+        y = [np.delete(y[i], j) for i in range(len(x)) for j in yy[i]]
+        return x, y
 
-    classification_x = [[] for _ in range(len(np.unique(clusters)))]
-    classification_y = [[] for _ in range(len(np.unique(clusters)))]
+    classification_x = [[] for _ in range(len(np.unique(clusters)) - 1)]
+    classification_y = [[] for _ in range(len(np.unique(clusters)) - 1)]
     year = [np.concatenate(data[month : month + 12]) for month in np.arange(data.size - 12)]
     medium_ = medium([year[i] for i in range(len(year))])
     attractorization_x, attractorization_y = henon(medium_, a=a, b=b)
+    attractorization_x, attractorization_y = clear(attractorization_x, attractorization_y)
+    for i in range(step):
+        x, y = [], []
+        for j in range(len(attractorization_x)):
+            x += [[1 - a * attractorization_x[j][k] ** 2 + attractorization_y[j][k] for k in range(len(attractorization_x[j]) - 1)]]
+            y += [[b * attractorization_x[j][k] for k in range(len(attractorization_x[j]) - 1)]]
+        attractorization_x = x
+        attractorization_y = y         
     for i in range(len(attractorization_x)):
-        classification_x[clusters[i]] += [attractorization_x[i]]
-        classification_y[clusters[i]] += [attractorization_y[i]]
+        if clusters[i] != -1:
+            classification_x[clusters[i]] += [attractorization_x[i]]
+            classification_y[clusters[i]] += [attractorization_y[i]]
     return classification_x, classification_y
+
+def meaning(data, clusters, step):
+    m_y = [[] for _ in range(len(np.unique(clusters)) - 1)]
+    m_x = [[] for _ in range(len(np.unique(clusters)) - 1)]
+    for i in range(step):
+        x,y = attractor_(data = data, clusters = clusters, step = i)
+        for j in range(len(x)):
+            m_x[j] += [np.mean(np.concatenate(x[j]))]
+            m_y[j] += [np.var(np.concatenate(x[j]))]
+    for i in range(len(m_x)):
+    #    sns.lineplot(x = range(len(m_x[i])), y = m_x[i])
+        sns.scatterplot(x = m_x[i], y = m_y[i])
+    plot.show()
 
 
 def mean(data, size):
@@ -110,7 +148,6 @@ def clear(
                 dx = round(dx, precision)
                 dy = round(dy, precision)
                 days += [(dx, dy)]
-            # concatenate days in a year
             years += days
         clusters += [tuple(years)]
     clusters = tuple(clusters)
@@ -147,3 +184,54 @@ def clear(
         result += [tuple(values)]
     result = tuple(result)
     return result
+
+def henon_heiles(data, max_energy, step):
+    def function(x, y, axis):
+        if axis == 0:
+            value = -x - 2*x*y
+        elif axis == 1:
+            value = -y -x**2 + y**2
+        return value
+
+    def Runge_Kutta(x, y, step, axis):
+        for i in np.arange(0, 8, step):
+            k_1 = function(x[i], y[i], axis = axis)
+            k_2 = function(x[i] + step/2, y[i] + step * k_1 / 2, axis = axis)
+            k_3 = function(x[i] + step/2, y[i] + step * k_2 / 2, axis = axis)
+            k_4 = function(x[i] + step, y[i] + step*k_3, axis = axis)
+            if axis == 1:
+                y[i+step] = y[i] + 1/6 * step*(k_1 + 2*k_2 + 2*k_3 + k_4)
+            elif axis == 0:
+                x[i+step] = x[i] + 1/6 * step*(k_1 + 2*k_2 + 2*k_3 + k_4)
+        if axis == 0:
+            return x
+        elif axis == 1:
+            return y
+
+    def energy(y):
+        return [1/2*y[p]**2*(1-2/3*y[p]) for p in range(len(y))]
+    
+    def foundation(y_original, y_integration):
+        result_x, result_y = [], []
+        for i in range(len(y_integration)-1):
+            if y_integration[i] < 0 and y_integration[i+1] > 0 or y_integration[i] > 0 and y_integration[i+1] < 0:
+                result_y += [y_integration[i] + y_integration[i+1]/2] 
+                result_x += [y_original[i]+y_original[i+1]/2] 
+        return result_x, result_y
+
+    year = [np.concatenate(data[month : month + 12]) for month in np.arange(data.size - 12)]
+    mediam = medium([year[i] for i in range(len(year))])
+    answer_x, answer_y, l = [], [], 0
+    for i in range(len(mediam)):
+        x, y = mediam[i][:-1], mediam[i][1:]
+        print(len(y))
+        energy = energy(y)
+        #print(energy)
+        for j in energy:
+            l += 1
+            if j <= max_energy:
+                answer_x += foundation(y[l:], Runge_Kutta(x[l:], y[l:], step, 1))[0]
+                answer_y += foundation(y[l:], Runge_Kutta(x[l:], y[l:], step, 1))[1]
+                print(answer_x)
+        l = 0
+    return answer_x, answer_y
--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.subplots as sp
+import seaborn as sns
+import matplotlib.pyplot as plot
+import plotly.express as px
 
 import functions
 
 markets = (
-    # "data/korea",
-    "data/london",
-    # "data/taiwan",
+#    "G:\Games\data\korea",
+#   "G:\Games\data\london",
+    "G:\Games\data\iwan",
 )
 
 figure = sp.make_subplots(rows=1, cols=len(markets))
@@ -25,6 +28,7 @@ for index, market in enumerate(markets):
     )
     stability_window_means = functions.window_means(stability_by_month)
     indexes = pd.read_csv(f"{market}/indexes.csv", header=0, parse_dates=["date"]).dropna()
+
     indexes_by_month = (
         indexes.groupby([clients["date"].dt.year, clients["date"].dt.month])["close"]
         .apply(np.array)
@@ -32,11 +36,29 @@ for index, market in enumerate(markets):
     )
     indexes_window_slopes = functions.window_slopes(indexes_by_month)
     clusters_ = functions.clusters(indexes_window_slopes, stability_window_means)
+    print(np.unique(clusters_))
+    #clusters_ = clusters_[0]
+    #print(len(clusters_))
+    print(len(clusters_))
+    print(len(indexes_by_month))
+#    x, y = functions.henon_heiles(indexes_by_month, 0.125, 1)
 
-    attractor = functions.attractor(indexes_by_month, clusters_, a=1.4, b=0.3)
-    for cluster in functions.clear(*attractor):
-        x = [i[0] for i in cluster]
-        y = [i[1] for i in cluster]
-        figure.add_trace(go.Scatter(x=x, y=y, mode="markers"), row=1, col=index + 1)
+#    x,y = functions.attractor_(indexes_by_month, clusters_, step = 4)
+    functions.meaning(indexes_by_month, clusters_, step = 20)
+#    for cluster in functions.clear(*attractor, 2):
+#        x = [i[0] for i in cluster]
+#        print("среднее:", np.mean(x))
+#        print(len(x))
+#        y = [i[1] for i in cluster]
+#        figure.add_trace(go.Scatter(x=x, y=y, mode="markers"), row=1, col=index + 1)
+#    for i in range(len(x)):
+#        print(np.mean(np.concatenate(x[i])))
+#        figure.add_trace(go.Scatter(x= np.concatenate(x[i]), y=np.concatenate(y[i]), mode="markers"), row=1, col=index + 1)
+    #figure.add_trace(go.Scatter(x= np.concatenate(x[1]), y=np.concatenate(y[1]), mode="markers"), row=1, col=index + 1)
+    #sns.scatterplot(x= stability_window_means, y = indexes_window_slopes, palette = clusters_)
+    #figure = px.scatter(x= stability_window_means, y = indexes_window_slopes, color=clusters_)
+    #figure.add_trace(go.Scatter(x= stability_window_means, y = indexes_window_slopes, mode="markers"), row = 1, col = index +1)
+#    figure = px.scatter(x= stability_window_means, y = indexes_window_slopes, color = clusters_)
 
-figure.show()
+#figure.show()
+#plot.show()
